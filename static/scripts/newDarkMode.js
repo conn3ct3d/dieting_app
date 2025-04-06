85ab7d62721dce7cf6d917c2447808b7359727a1
Emiliano Magana <websmithmxl@gmail.com>
2025-04-05 22:12:51 -0700

    fixed missing static loading

--- static/scripts/newDarkMode.js
+++ static/scripts/newDarkMode.js
@@ -1,13 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const toggleBtn = document.getElementById("theme-toggle");
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const isDark = localStorage.getItem('dark-mode' === 'true');
+    const isDark = localStorage.getItem('dark-mode') === 'true';
 
-    if (prefersDark || isDark ) {
-        document.body.classList.add('dark.mode');
+    if (prefersDark || isDark) {
+      document.body.classList.add('dark-mode');
     }
-    toggleBtn.addEventListener("click",()=>{
-        document.body.classList.toggle("dark-mode");
-        localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
-    }
-})
+
+    toggleBtn.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+      localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode'));
+    });
+  });
